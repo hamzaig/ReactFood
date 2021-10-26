4172c4e97387082c92b92f60c844d79571b01b4c
hamzaig <hamzaig@yahoo.com>
2021-10-26 20:58:27 +0500

    Changing the cart numbers according to items adding

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import CartContext from '../../store/cart-context';
 import CartIcon from "../Cart/CartIcon"
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+    const cartCtx = useContext(CartContext);
+
+    const numberOfCartItems = cartCtx.items.reduce((acc, elem) => {
+        return acc + elem.amount;
+    }, 0);
+
     return (
         <>
             <button className={classes.button} onClick={props.onClick}>
@@ -10,7 +17,7 @@ const HeaderCartButton = (props) => {
                     <CartIcon />
                 </span>
                 <span>Your Cart</span>
-                <span className={classes.badge}>3</span>
+                <span className={classes.badge}>{numberOfCartItems}</span>
             </button>
         </>
     )
